7e80db8e4abe95ecbc475c6b0dafb9a2b44fdf7a
Lokesh <63948472+LokeshNaidu8@users.noreply.github.com>
2021-12-24 12:44:23 +0530

    1.0

---
+++ fireship.js
@@ -0,0 +1,37 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAHDpL9rHR53imz417GQb-jnT6EwlEXVZs",
+    authDomain: "gleansmart.firebaseapp.com",
+    databaseURL: "https://gleansmart.firebaseio.com",
+    projectId: "gleansmart",
+    storageBucket: "gleansmart.appspot.com",
+    messagingSenderId: "520429925831",
+    appId: "1:520429925831:web:b024c639e53eb2a01695e2",
+    measurementId: "G-KF8W9FLDGG"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(
+        m,
+        key,
+        value
+    ) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+let PROJECT_ID = getUrlVars()["projectid"];
+
+console.log(PROJECT_ID);
+
+// db.collection("Projects").get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         console.log(`${doc.id} => ${doc.data()}`);
+//     });
+// });
